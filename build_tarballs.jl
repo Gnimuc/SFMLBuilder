5acd03c10454164a10fa702cda0ad7f15fcb9d75
Gnimuc <qiyupei@gmail.com>
2020-10-01 17:14:21 +0800

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -7,15 +7,14 @@ version = v"2.5.1"
 
 # Collection of sources required to build SFML
 sources = [
-    "https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz" =>
-    "34ad106e4592d2ec03245db5e8ad8fbf85c256d6ef9e337e8cf5c4345dc583dd",
-
-    "https://github.com/SFML/SFML.git" =>
-    "192eb968a4e938f36948e97f97ddc354a8a470fe",
-
-    "https://github.com/SFML/CSFML.git" =>
-    "61f17e3c1d109b65ef7e3e3ea1d06961da130afc",
-
+    GitSource(
+        "https://github.com/SFML/SFML.git",
+        "2f11710abc5aa478503a7ff3f9e654bd2078ebab",
+    ),
+    ArchiveSource(
+        "https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz",
+        "34ad106e4592d2ec03245db5e8ad8fbf85c256d6ef9e337e8cf5c4345dc583dd",
+    ),
 ]
 
 # Bash recipe for building across all platforms
@@ -28,13 +27,14 @@ if [[ "${target}" == *linux* ]]; then
 cd SFML-2.5.1/
 mv ./include $WORKSPACE/destdir/
 mv ./lib $WORKSPACE/destdir/
+mv ./share $WORKSPACE/destdir/
 
 else
 
 cd SFML
 mkdir build && cd build
 
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=/opt/${target}/${target}.toolchain"
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}"
 
 if [[ "${target}" == *apple* ]]; then
 CMAKE_FLAGS="${CMAKE_FLAGS} -DSFML_DEPENDENCIES_INSTALL_PREFIX=${WORKSPACE}/destdir/Frameworks"
@@ -51,55 +51,32 @@ fi
 cmake .. ${CMAKE_FLAGS}
 make
 make install
+install_license ../license.md 
 
 fi
-
-# build CSFML
-cd ${WORKSPACE}/srcdir
-cd CSFML
-mkdir build && cd build
-
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=/opt/${target}/${target}.toolchain"
-CMAKE_FLAGS="${CMAKE_FLAGS} -DSFML_DIR=${WORKSPACE}/destdir/lib/cmake/SFML"
-
-if [[ "${target}" == *mingw* ]]; then
-CMAKE_FLAGS="${CMAKE_FLAGS} -DCSFML_LINK_SFML_STATICALLY=false"
-fi
-
-cmake .. ${CMAKE_FLAGS}
-make
-make install
 
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
-    MacOS(:x86_64),
-    Windows(:x86_64; compiler_abi=CompilerABI(:gcc7)),
-    Windows(:i686; compiler_abi=CompilerABI(:gcc7)),
+    Linux(:x86_64, libc=:glibc), 
+    MacOS(:x86_64), 
+    Windows(:i686), 
+    Windows(:x86_64),
 ]
 
-
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, ["libcsfml-graphics", "csfml-graphics-2"], :libcsfml_graphics),
-    LibraryProduct(prefix, ["libsfml-window", "sfml-window-2"], :libsfml_window),
-    LibraryProduct(prefix, ["libsfml-audio", "sfml-audio-2"], :libsfml_audio),
-    LibraryProduct(prefix, ["libsfml-network", "sfml-network-2"], :libsfml_network),
-    LibraryProduct(prefix, ["libsfml-system", "sfml-system-2"], :libsfml_system),
-    LibraryProduct(prefix, ["libsfml-graphics", "sfml-graphics-2"], :libsfml_graphics),
-    LibraryProduct(prefix, ["libcsfml-system", "csfml-system-2"], :libcsfml_system),
-    LibraryProduct(prefix, ["libcsfml-network", "csfml-network-2"], :libcsfml_network),
-    LibraryProduct(prefix, ["libcsfml-window", "csfml-window-2"], :libcsfml_window),
-    LibraryProduct(prefix, ["libcsfml-audio", "csfml-audio-2"], :libcsfml_audio)
+products = [
+    LibraryProduct(["libsfml-window", "sfml-window-2"], :libsfml_window),
+    LibraryProduct(["libsfml-audio", "sfml-audio-2"], :libsfml_audio),
+    LibraryProduct(["libsfml-network", "sfml-network-2"], :libsfml_network),
+    LibraryProduct(["libsfml-system", "sfml-system-2"], :libsfml_system),
+    LibraryProduct(["libsfml-graphics", "sfml-graphics-2"], :libsfml_graphics),
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-
-]
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
